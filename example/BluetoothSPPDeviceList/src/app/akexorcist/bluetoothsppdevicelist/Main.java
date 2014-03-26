558591f79daa0f589de450cdcc11f808019854fd
Akexorcist <akexorcist@gmail.com>
2014-03-26 13:56:52 +0700

    Update Main.java

--- example/BluetoothSPPDeviceList/src/app/akexorcist/bluetoothsppdevicelist/Main.java
+++ example/BluetoothSPPDeviceList/src/app/akexorcist/bluetoothsppdevicelist/Main.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Akexorcist
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 package app.akexorcist.bluetoothsppdevicelist;
 
 import android.os.Bundle;
@@ -45,7 +61,7 @@ public class Main extends Activity {
                     Intent intent = new Intent(Main.this, DeviceList.class);
                     intent.putExtra("bluetooth_devices", "Bluetooth devices");
                     intent.putExtra("no_devices_found", "No device");
-                    intent.putExtra("scanning", "¡ÓÅÑ§·Ó¡ÒÃ¤é¹ËÒ");
+                    intent.putExtra("scanning", "ï¿½ï¿½ï¿½Ñ§ï¿½Ó¡ï¿½Ã¤ï¿½ï¿½ï¿½ï¿½");
                     intent.putExtra("scan_for_devices", "Search");
                     intent.putExtra("select_device", "Select");
                     intent.putExtra("layout_list", R.layout.bluetooth_layout_list);
